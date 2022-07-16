0aedc447f4fd77362255f1cac4c9c100115693ba
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2022-07-16 19:11:08 -0300

    Escapando texto através de Decorator

---
+++ app/dist/js/decorators/escape.js
@@ -0,0 +1,11 @@
+export function Escape(target, propertyKey, descriptor) {
+    const metodoOriginal = descriptor.value;
+    descriptor.value = function (...args) {
+        let retorno = metodoOriginal.apply(this, args);
+        if (typeof retorno === 'string') {
+            console.log(`@escape em ação na classe ${this.constructor.name} para o método ${propertyKey}`);
+            retorno = retorno.replace(/<script>[\s\S]*?<\/script>/, '');
+        }
+    };
+    return descriptor;
+}
---
+++ app/src/decorators/escapar.ts
@@ -0,0 +1,16 @@
+export function escapar(
+    target: any,
+    propertyKey: String,
+    descriptor: PropertyDescriptor
+) {
+    const metodoOriginal = descriptor.value;
+    descriptor.value = function(...args: any[]) {
+        let retorno = metodoOriginal.apply(this, args);
+        if(typeof retorno === 'string') {
+            console.log(`@escape em ação na classe ${this.constructor.name} para o método ${propertyKey}`);
+            
+            retorno = retorno.replace(/<script>[\s\S]*?<\/script>/, '');
+        }
+    }
+    return descriptor; 
+}
