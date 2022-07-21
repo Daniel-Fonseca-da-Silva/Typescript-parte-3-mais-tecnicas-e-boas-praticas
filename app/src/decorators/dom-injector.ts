13eb7e71d8bf184feb0851aa2a2b84d8c9a986e1
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2022-07-21 01:06:37 -0300

    Criando getters dinamicamente

--- app/src/decorators/dom-injector.ts
+++ app/src/decorators/dom-injector.ts
@@ -1,3 +1,10 @@
 export function domInjector(seletor: string) {
-    
+    return function(target: any, propertyKey: string) {
+        const getter = function() {
+            const elemento = document.querySelector(seletor);
+            return elemento;
+        }
+        
+    }
 }
+
