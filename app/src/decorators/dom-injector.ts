50101e9acd054eadd44529c7731d6f182f60ef4e
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2022-07-22 01:14:55 -0300

    Aplicando cache decorator copy

--- app/src/decorators/dom-injector.ts
+++ app/src/decorators/dom-injector.ts
@@ -1,10 +1,13 @@
 export function domInjector(seletor: string) {
     return function(target: any, propertyKey: string) {
         console.log(`Modificando prototype ${target.constructor.name} e adicionando getter para a propriedade ${propertyKey}`);
-        
+        let elemento: HTMLElement | null = null;
+
         const getter = function() {
-            const elemento = document.querySelector(seletor);
-            console.log(`Buscando elemento do DOM com o seletor ${seletor} para injetar em ${propertyKey}`);
+            if(!elemento) {
+                elemento = <HTMLElement>document.querySelector(seletor);
+                console.log(`Buscando elemento do DOM com o seletor ${seletor} para injetar em ${propertyKey}`);
+            }
             return elemento;
         }
 
